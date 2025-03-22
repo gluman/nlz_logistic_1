5419544bf4ca64ed7f77425bef0004d572825d68
Andrey Glumov <gluman@yandex.ru>
2025-03-22 22:12:01 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+    
+    const selector = document.getElementById('packetSelector');
+    const listPackets = document.querySelector('.list_packets');
+    const allTara = document.querySelectorAll('.tara')
+    const packets = document.querySelectorAll('.packet')
+
+    packets.forEach(packet => {
+        packet.addEventListener('click', () => {
+            
+        })
+    })
+
